6b2bd71eaaa3bc90b4118bb4dec5382ebf122fb4
Gaspard Loriot-Boserup <gaspardloriot@gmail.com>
2021-11-18 08:34:23 +0000

    feat: add styled-divs and redux-toolkit boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
+const Button = styled.a`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+
+  /* The GitHub button is a primary button
+   * edit this to target it specifically! */
+  ${`
+    background: white;
+    color: black;
+  `}
+`;
 
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
+        <h1> The count is: {count}</h1>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(33))}>
+          Increment by 33
+        </button>
+        <Button>click me</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
