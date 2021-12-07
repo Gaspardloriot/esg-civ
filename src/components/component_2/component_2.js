ad2773e7df31da835c7af8b39214c4e3794c4ba9
Gaspard Loriot-Boserup <gaspardloriot@gmail.com>
2021-12-07 11:20:37 +0000

    feat : add spinner

--- src/components/component_2/component_2.js
+++ src/components/component_2/component_2.js
@@ -18,7 +18,7 @@ export const Component2 = () => {
       <DashboardContainer fluid>
         <MetaBaseDashboard
           title="myFrame"
-          src="https://esg-analytics.herokuapp.com/embed/dashboard/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE2Mzg3NTAxNDB9.eN-kifIgAx7FEZPqCMt4yKa9RePUybMun6H1eNRXmQc#theme=night&bordered=true&titled=true"
+          src="https://esg-analytics.herokuapp.com/embed/dashboard/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE2Mzg4ODI5NjJ9.K1MYzXsxq2nmlq-Isy48lgQyr4SIAo-HnmFC8kdBHbY#theme=night&bordered=true&titled=true"
           width="100%"
           height="2000px"
         ></MetaBaseDashboard>
--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,18 +1,64 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Navigate } from "react-router";
-import {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { changeAuth } from "../../redux/reducer";
 import { auth } from "../../security/firebase";
+import { Container, Alert, Button, Spinner } from "react-bootstrap";
+import styled from "styled-components";
+import { mainColors as styles } from "../../globalStyles";
+import logo from "../../containers/Header/icons8-nordvpn.svg";
+
+const Page = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${styles.main.appBackground};
+  min-height: 100vh;
+  color: white;
+`;
+
+const MainContainer = styled.div`
+  background-color: ${styles.header.headerBackground};
+  border: 1px solid ${styles.metabase.metabaseBlue};
+  padding: 15px;
+  border-radius: 4px;
+  width: 30%;
+`;
+
+const LogInput = styled.input`
+  padding: 7px;
+  margin: 8px 20px;
+  background-color: transparent;
+  border-radius: 0px;
+  border: 1px solid transparent;
+  border-bottom: 2px solid white;
+  color: white;
+  ::placeholder {
+    /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: white;
+    font-weight: 100;
+    opacity: 0.8; /* Firefox */
+  }
+
+  :-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: red;
+  }
+
+  ::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: red;
+  }
+`;
 
 export const Login = () => {
   const dispatch = useDispatch();
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [alert, setAlert] = useState({ show: false, content: "" });
+  const [spinnerDisplay, setSpinnerDisplay] = useState("none");
   const currentUser = auth.currentUser;
 
   onAuthStateChanged(auth, (currentUser) => {
@@ -34,42 +80,81 @@ export const Login = () => {
 
   const login = async () => {
     try {
+      setSpinnerDisplay("");
       await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
       //const idToken = await currentUser.getIdToken(/* forceRefresh */ true);
       //console.log(idToken);
     } catch (error) {
-      console.log(error.message);
+      setSpinnerDisplay("none");
+      setAlert({
+        show: true,
+        content: error.message.replace("Firebase: ", ""),
+      });
     }
   };
 
-  const logout = async () => {
-    await signOut(auth);
-  };
-
   return (
-    <div>
-      <div>
-        <h3> Login </h3>
-        <input
-          placeholder="Email..."
-          onChange={(event) => {
-            setLoginEmail(event.target.value);
-          }}
-        />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setLoginPassword(event.target.value);
-          }}
-        />
-
-        <button onClick={login}> Login</button>
+    <Page fluid>
+      <div
+        style={{
+          width: "30%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          marginBottom: "15px",
+          backgroundColor: "white",
+          borderRadius: "4px",
+          color: "black",
+          border: "1px solid black",
+        }}
+      >
+        <div style={{ justifyContent: "center", display: "flex" }}>
+          <img
+            src={logo}
+            width="100"
+            height="auto"
+            style={{ backgroundColor: "white", borderRadius: "4px" }}
+          />
+        </div>
+        <p style={{ textAlign: "center", fontSize: "25px", fontWeight: "100" }}>
+          ESG Analytics
+        </p>
       </div>
-
-      <h4> User Logged In: </h4>
-      {currentUser?.email}
-
-      <button onClick={logout}> Sign Out </button>
-    </div>
+      <MainContainer>
+        <Alert key={1} variant="danger" show={alert.show}>
+          {alert.content}
+        </Alert>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <LogInput
+            placeholder="Email..."
+            onChange={(event) => {
+              setLoginEmail(event.target.value);
+            }}
+          />
+          <LogInput
+            type="password"
+            placeholder="Password..."
+            onChange={(event) => {
+              setLoginPassword(event.target.value);
+            }}
+          />
+          <Button
+            onClick={login}
+            variant="light"
+            style={{ margin: "10px 100px", marginTop: "10px" }}
+          >
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+              style={{ display: spinnerDisplay }}
+            />{" "}
+            Login
+          </Button>
+        </div>
+      </MainContainer>
+    </Page>
   );
 };
